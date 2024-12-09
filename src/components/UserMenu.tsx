babcce7a01297f2865cd846b0b39e3af535cc21a
LihayM <LihayMorad@gmail.com>
2024-12-09 23:44:43 +0200

    update UserMenu placeholder

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,4 +1,5 @@
-import { styled, IconButton } from "@mui/material";
+import { useState } from "react";
+import { styled, IconButton, Menu, MenuItem } from "@mui/material";
 import { AccountCircle } from "@mui/icons-material";
 
 const StyledIconButton = styled(IconButton)`
@@ -8,11 +9,32 @@ const StyledIconButton = styled(IconButton)`
 `;
 
 const UserMenu = () => {
+  const [menuAnchorEl, setMenuAnchorEl] = useState<null | HTMLElement>(null);
+  const isOpen = !!menuAnchorEl;
+
+  const openMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setMenuAnchorEl(event.currentTarget);
+  };
+  const closeMenu = () => {
+    setMenuAnchorEl(null);
+  };
+
   return (
     <>
-      <StyledIconButton color="primary" onClick={() => {}}>
+      <StyledIconButton color="primary" onClick={openMenu}>
         <AccountCircle fontSize="large" color="inherit" />
       </StyledIconButton>
+
+      <Menu
+        anchorEl={menuAnchorEl}
+        open={isOpen}
+        onClose={closeMenu}
+        keepMounted
+      >
+        <MenuItem>AAA</MenuItem>
+        <MenuItem>BBB</MenuItem>
+        <MenuItem>CCC</MenuItem>
+      </Menu>
     </>
   );
 };
