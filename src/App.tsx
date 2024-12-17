2d9cfda1e41f3c342f7c0ffd629ba23b58a91026
LihayM <LihayMorad@gmail.com>
2024-12-17 23:35:55 +0200

    wrap the app with StyledEngineProvider so Mui styles can be overriden

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
+import { StyledEngineProvider } from "@mui/material/styles";
 import "./styles/main.scss";
 import Logo from "./components/Logo";
 import UserMenu from "./components/UserMenu/UserMenu";
+import FiltersMenu from "./components/FiltersMenu/FiltersMenu";
 import UserContextProvider from "./context/authContext";
 import FiltersContextProvider from "./context/filtersContext";
 
 function App() {
   return (
-    <UserContextProvider>
-      <FiltersContextProvider>
-        <Logo />
-        <UserMenu />
-      </FiltersContextProvider>
-    </UserContextProvider>
+    <StyledEngineProvider injectFirst>
+      <UserContextProvider>
+        <FiltersContextProvider>
+          <Logo />
+          <UserMenu />
+          <FiltersMenu />
+        </FiltersContextProvider>
+      </UserContextProvider>
+    </StyledEngineProvider>
   );
 }
 
