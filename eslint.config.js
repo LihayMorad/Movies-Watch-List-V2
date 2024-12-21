c0f22d93d9639bcbaefca312a165f3099efec1aa
LihayM <LihayMorad@gmail.com>
2024-12-21 21:00:46 +0200

    add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import react from 'eslint-plugin-react'
+import prettierConfig from 'eslint-config-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -34,4 +35,5 @@ export default tseslint.config(
       ],
     },
   },
+  prettierConfig
 )
