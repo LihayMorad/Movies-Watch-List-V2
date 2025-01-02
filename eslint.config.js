7f18b647e132f360b2b8524bccdc7e7ecfb1a3d3
LihayM <LihayMorad@gmail.com>
2025-01-02 21:51:08 +0200

    disable eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -30,6 +30,7 @@ export default tseslint.config(
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "@typescript-eslint/no-misused-promises": ["error", { checksVoidReturn: false }],
     },
   },
   prettierConfig
