54b63dc751702836ae07a52f83085a7cea2a0ed0
LihayM <LihayMorad@gmail.com>
2025-01-02 23:04:13 +0200

    update Navigation Menu

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,19 +1,35 @@
 import { Grid2, Link } from "@mui/material";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
 import WhatshotIcon from "@mui/icons-material/Whatshot";
+import { useState } from "react";
+
+const WATCHLIST = "WATCHLIST";
+const TRENDING = "TRENDING";
 
 const NavigationMenu = () => {
+  const [activeRoute, setActiveRoute] = useState(WATCHLIST);
+
   return (
-    <Grid2 container alignItems="center" gap="24px">
+    <Grid2 container alignItems="center" gap="24px" height="50px" paddingX="24px" borderRadius="50px" sx={{ backgroundColor: "#091c2f" }}>
       <Grid2>
-        <Link variant="subtitle1" href="#" underline="hover">
+        <Link
+          variant="subtitle1"
+          onClick={() => setActiveRoute(WATCHLIST)}
+          underline={activeRoute === WATCHLIST ? "always" : "hover"}
+          color="#ffffff"
+        >
           <SubscriptionsIcon fontSize="inherit" sx={{ marginRight: "4px" }} />
           Watchlist
         </Link>
       </Grid2>
 
       <Grid2>
-        <Link variant="subtitle1" href="#" underline="hover" color="warning">
+        <Link
+          variant="subtitle1"
+          onClick={() => setActiveRoute(TRENDING)}
+          underline={activeRoute === TRENDING ? "always" : "hover"}
+          color="warning"
+        >
           <WhatshotIcon fontSize="inherit" sx={{ marginRight: "4px" }} />
           Trending
         </Link>
