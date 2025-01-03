a2c9a3352cddff1d8d8f0662d5133161d2ec824b
LihayM <LihayMorad@gmail.com>
2025-01-04 00:13:04 +0200

    move type to types folder

---
+++ src/types/Movie.ts
@@ -0,0 +1,17 @@
+export interface TrendingMovie {
+  backdrop_path: string;
+  id: number;
+  title: string;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  media_type: string;
+  adult: boolean;
+  original_language: string;
+  genre_ids: number[];
+  popularity: number;
+  release_date: string; // "2024-11-20";
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
